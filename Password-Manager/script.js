44ab7440762105946e26d74c5c3a7d0159239ab1
Divyanshu Dugar <divyu2004@gmail.com>
2024-05-06 19:24:39 -0400

    added email option

--- Password-Manager/script.js
+++ Password-Manager/script.js
@@ -1,23 +1,32 @@
-console.log("Working with JS");
-
-// Logic to fill the table
-let tb = document.querySelector("table");
-let data = localStorage.getItem("passwords");
-if (data) {
-  let arr = JSON.parse(data);
-  for (let index = 0; index < arr.length; index++) {
-    const element = arr[index];
-    let str = `<tr>
-                    <td>${element.website}</td>
-                    <td>${element.username}</td>
-                    <td>${element.password}</td>
-                    <td>${"Delete"}</td>
-                   </tr>`;
-    tb.innerHTML = tb.innerHTML + str;
+const showPasswords = () => {
+  // Logic to fill the table
+  let tb = document.querySelector("table");
+  let data = localStorage.getItem("passwords");
+  if (data) {
+    tb.innerHTML = `        <tr>
+        <th>Website</th>
+        <th>Username</th>
+        <th>Password</th>
+        <th>Delete</th>
+      </tr>`;
+    let arr = JSON.parse(data);
+    for (let index = 0; index < arr.length; index++) {
+      const element = arr[index];
+      let str = `<tr>
+            <td>${element.website}</td>
+            <td>${element.username}</td>
+                        <td>${element.password}</td>
+                        <td>${"Delete"}</td>
+                        </tr>`;
+      tb.innerHTML = tb.innerHTML + str;
+    }
+  } else {
+    tb.innerHTML = "No Data To Show";
   }
-} else {
-  tb.innerHTML = "No Data To Show";
-}
+};
+
+console.log("Working with JS");
+showPasswords();
 
 document.querySelector(".btn").addEventListener("click", (e) => {
   e.preventDefault();
@@ -30,15 +39,24 @@ document.querySelector(".btn").addEventListener("click", (e) => {
     // Retrieve JSON-formatted passwords from local storage and parse them into a JavaScript object.
     let json = JSON.parse(localStorage.getItem("passwords"));
     // Add a new object containing a username and password to the JSON array.
-    json.push({ username: username.value, password: password.value });
+    json.push({
+      website: website.value,
+      username: username.value,
+      password: password.value,
+    });
     // Display an alert indicating that the password has been saved.
     alert("Password Saved!");
     // Convert the updated JSON array back into a JSON string and store it in local storage under the key "passwords".
     localStorage.setItem("passwords", JSON.stringify(json));
   } else {
     let json = [];
-    json.push({ username: username.value, password: password.value });
+    json.push({
+      website: website.value,
+      username: username.value,
+      password: password.value,
+    });
     alert("Password Saved!");
     localStorage.setItem("passwords", JSON.stringify(json));
   }
+  showPasswords();
 });
